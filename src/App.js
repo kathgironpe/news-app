5eaf44b7eaab783eb3b5efb59fc3a86ff54e6056
Katherine G. Pe <katherinepe@me.com>
2018-06-17 14:47:20 +1000

    Added React motion and Semantic UI

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">A News App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
