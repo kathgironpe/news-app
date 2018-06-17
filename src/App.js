3caa299ef07425f311d6e56b53f44ec24e6ff2cd
Katherine G. Pe <katherinepe@me.com>
2018-06-17 14:23:43 +1000

    Install Redux Thunk and Axios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">A News App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
