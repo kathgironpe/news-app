72d3468bbb1ce61fd0bb085cf235330ad07e2fcf
Katherine G. Pe <katherinepe@me.com>
2018-06-21 22:21:17 +1000

    Updated the app bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import React, { Component, Fragment } from 'react';
 import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ padding: 20 }}>
-        <header className="App-header">
-        </header>
-        <Grid container spacing={40}>
-        </Grid>
-      </div>
+      <Fragment>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Button>Australia</Button>
+            <Button>United States</Button>
+          </Toolbar>
+        </AppBar>
+        <div className="App" style={{ padding: 20 }}>
+          <Grid container spacing={40}>
+          </Grid>
+        </div>
+     </Fragment>
     );
   }
 }
