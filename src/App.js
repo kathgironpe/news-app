95dfc5b7269139a975cb631ba3388dc06fdb9a5c
Katherine G. Pe <katherinepe@me.com>
2018-06-21 21:25:40 +1000

    Installed material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ padding: 20 }}>
         <header className="App-header">
-          <h1 className="App-title">A News App</h1>
+          <Button variant="raised" color="primary">
+            Hello World
+          </Button>
         </header>
+        <Grid container spacing={40}>
+        </Grid>
       </div>
     );
   }
