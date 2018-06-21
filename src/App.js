8d6a0b28b4296cef38e6b990f31ef5849f78c351
Katherine G. Pe <katherinepe@me.com>
2018-06-21 22:01:24 +1000

    Configured webpack to use SCSS loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
